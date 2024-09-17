ebde5da1669dda0ae638fe0c48cdab47d37963e8
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-17 11:31:24 -0300

    feat: add header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import { Header } from './components/header';
+
 export function App() {
-	return <h1>Hello, Prop Drilling!</h1>;
+	return (
+		<div className="space-y-8 min-h-screen bg-zinc-900 text-zinc-100">
+			<Header />
+		</div>
+	);
 }
