e979dddc2047a4953aceaeb81acb59c943459809
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-17 11:50:04 -0300

    feat: add users list component to show created users

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Header } from './components/header';
+import { UsersList } from './components/users-list';
 
-type User = {
+export type User = {
 	id: number;
 	name: string;
 	email: string;
@@ -23,6 +24,13 @@ export function App() {
 	return (
 		<div className="space-y-8 min-h-screen bg-zinc-900 text-zinc-100">
 			<Header addUser={addUser} />
+
+			<main className="container mx-auto">
+				<div className="flex flex-col gap-4">
+					<h1 className="text-2xl font-semibold">Users</h1>
+					<UsersList users={users} />
+				</div>
+			</main>
 		</div>
 	);
 }
